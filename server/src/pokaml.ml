acc8b71721f60cc1c2f5ae91fd0dcbff5cfdd66e
Joris <joris.guyonvarch@gmail.com>
2016-11-24 12:10:48 +0100

    Serve main and assets

--- server/src/pokaml.ml
+++ server/src/pokaml.ml
@@ -2,18 +2,30 @@ open Core.Std
 open Async.Std
 open Cohttp_async
 
-open Foo
+let asset path = "./public/" ^ path
+
+let handler ~body:_ _sock req =
+  let uri = Cohttp.Request.uri req in
+  match Uri.path uri with
+  | "/" ->
+      Server.respond_with_file (asset "main.html")
+  | path when (String.is_prefix path "/assets/") ->
+      let assetsPath = asset (String.drop_prefix path (String.length "/assets/")) in
+      Server.respond_with_file assetsPath
+  | _ ->
+      Server.respond_with_string ~code:`Not_found "Route not found"
 
 let start_server port () =
   eprintf "Listening for HTTP on port %d\n" port;
-  eprintf "Try 'curl -X POST -d 'foo bar' http://localhost:%d\n" port;
   Cohttp_async.Server.create ~on_handler_error:`Raise
-    (Tcp.on_port port) (fun ~body _ req ->
-      match req |> Cohttp.Request.meth with
-      | `POST ->
-        (Body.to_string body) >>= (fun body ->
-          Log.Global.info "Body: %s" body;
-          Server.respond `OK)
-      | _ -> Server.respond `Method_not_allowed
-    )
->>= fun _ -> Deferred.never ()
+    (Tcp.on_port port) handler
+  >>= fun _ -> Deferred.never ()
+
+let () =
+  Command.async
+    ~summary:"Start a hello world Async server"
+    Command.Spec.(empty +>
+      flag "-p" (optional_with_default 8080 int)
+        ~doc:"int Source port to listen on"
+    ) start_server
+      |> Command.run
